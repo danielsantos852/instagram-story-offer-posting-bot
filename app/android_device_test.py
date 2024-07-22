a868693d1ec1afb38c9d3214b865dd577520f488
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-07-22 17:17:56 -0300

    Update android_device_test.py
    Changes:
    - added a few tests.

--- app/android_device_test.py
+++ app/android_device_test.py
@@ -5,17 +5,30 @@ import time
 
 # The Main function
 def main():
-    
-    # Connect to phone
-    phone = get_android_device(device_name='Poco X3 NFC')
-    print(f'\n{phone}\n')
 
-    # Launch Instagram app
-    phone.launch_instagram_app(force_restart=True)
-    time.sleep(4)
+    test_find_on_screen()
 
-    # Take a screenshot
-    print(phone.take_screenshot())
+
+# Test find_on_screen() function
+def test_find_on_screen():
+
+    # Get AndroidDevice object
+    phone = get_android_device()
+    print(f'{phone}')
+
+    # Find some image
+    phone.find_on_screen(image_subset='./resources/00a_btn_newstory.png',
+                         image_set=None,
+                         confidence_lvl=0.9)
+
+
+# Test pyautogui.locate() function
+def test_pyautogui_locate():
+    from pyautogui import locate
+    box = locate(needleImage='./resources/00a_btn_newstory.png',
+                 haystackImage='./resources/00_ss.png',
+                 confidence=0.9)
+    print(f'Image subset found at {box}')
 
 
 # Call main function
