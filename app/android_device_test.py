8f7a48e5c9513644f0da563164c8e905281d74e1
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-07-22 18:23:03 -0300

    Update android_device_test.py
    Added some tests.

--- app/android_device_test.py
+++ app/android_device_test.py
@@ -1,28 +1,48 @@
 # Imports
 from android_device import get_android_device
+import pyscreeze
 import time
 
 
 # The Main function
 def main():
 
-    test_find_on_screen()
+    test_screen_tap()
 
 
-# Test find_on_screen() function
+# Test Screen Tap
+def test_screen_tap():
+
+    # Get AndroidDevice object
+    phone = get_android_device()
+
+    # Launch Instagram app and wait a bit
+    phone.launch_instagram_app(force_restart=True)
+    time.sleep(4)
+
+    # Find "Add Story" button on screen
+    add_story_button = phone.find_on_screen(image_subset='./resources/00a_btn_newstory.png')
+
+    # Tap on button box
+    phone.screen_tap(tap_box=add_story_button)
+
+
+# Test Find On Screen
 def test_find_on_screen():
 
     # Get AndroidDevice object
     phone = get_android_device()
-    print(f'{phone}')
 
     # Find some image
-    phone.find_on_screen(image_subset='./resources/00a_btn_newstory.png',
-                         image_set=None,
-                         confidence_lvl=0.9)
+    box = phone.find_on_screen(image_subset='./resources/00a_btn_newstory.png',
+                               image_set=None,
+                               confidence_lvl=0.9)
+
+    print(box)
+    print(type(box))
 
 
-# Test pyautogui.locate() function
+# Test pyautogui.locate()
 def test_pyautogui_locate():
     from pyautogui import locate
     box = locate(needleImage='./resources/00a_btn_newstory.png',
