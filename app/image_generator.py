1655783cc8e21820c880b813fd1720c3630eff01
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-01 15:54:47 -0300

    Update image_generator.py
    Changes:
    - added templates for class methods;
    - other minor improvements.

--- app/image_generator.py
+++ app/image_generator.py
@@ -1,5 +1,6 @@
 # Imports
 import logging
+from PIL import Image
 
 
 # Logger configuration
@@ -11,22 +12,42 @@ handler.setFormatter(fmt=formatter)
 logger.addHandler(hdlr=handler)
 
 
-# Global configuration
-
-
 # The Generator class:
 class Generator:
 
-    # __init__ method
-    def __init__(self):
+    # Class logger setup
+    _class_logger = logging.getLogger(__name__).getChild(__qualname__)
+    _class_logger.info('Class logger set up.')
+
+
+    # --- Public Methods ---
+    # Generate offer image
+    @classmethod
+    def generate_offer_image(cls):
         ...
 
-    # __str__ method
-    def __str__(self) -> str:
+
+    # --- Helper Methods ---
+    # Create image from template
+    @classmethod
+    def _create_image_from_template(cls):
         ...
 
 
-# Get Generator function:
-def get_generator():
-    ...
+    # Insert image
+    @classmethod
+    def _insert_image(cls):
+        ...
+
+
+    # Insert textbox
+    @classmethod
+    def _insert_textbox(cls):
+        ...
+
+
+    # Save image as
+    @classmethod
+    def _save_image_as(cls):
+        ...
 
