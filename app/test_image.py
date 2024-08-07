987a89a36ab86f0ed2487d98cb7b9e0657af5598
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-07 18:34:25 -0300

    Update test_image.py

--- app/test_image.py
+++ app/test_image.py
@@ -1,34 +1,19 @@
 # Imports
-from image import Generator
+from image import Generator as ImageGenerator
 
 
-# Main Function
+# Main function
 def main():
 
     # Generate a fake offer image
-    Generator.create_instagram_offer_image(template_path='./resources/templates/story-720x1280-gray-white.png',
-                                           offer_thumbnail_path='./resources/fake/offer-thumbnail-640x640.png',
-                                           offer_title='Cool shoes',
-                                           offer_price_from=399.99,
-                                           offer_price_for=299.99,
-                                           dest_path='./temp/test-offer-image.png')
-
-
-# Test new image from template
-def test_new_image_from_template(file_path:str):
-    
-    # Create new image from template
-    Generator._new_image_from_template(file_path=file_path)
-
-    # Return nothing
-    return None
-
-
-# Test save image as
-def test_save_image_as(file_path:str) -> str:
-    
-    # Save generator's current image and return file path
-    return Generator._save_image_as(file_path=file_path)
+    ImageGenerator.create_instagram_offer_image(
+        template_path='./resources/templates/story-720x1280-gray-white.png',
+        offer_thumbnail_path='./resources/fake/offer-thumbnail-640x640.png',
+        offer_title='Cool shoes',
+        offer_price_from=1000.00,
+        offer_price_for=500.00,
+        dest_path='./temp/test-offer-image.png'
+    )
 
 
 # Call main function
