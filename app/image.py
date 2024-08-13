8cb4cd5c5086c759921579a1d809abb66a31ef70
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-13 16:57:47 -0300

    Update image.py
    Changes:
    - turned class methods into instance methods (except Generator.get());
    - improved Generator._add_offer_title();
    - improved Generator._add_offer_price();
    - improved Generator._save_image_as();
    - renamed Generator._load_image() as Generator._new_image_from_template() for clarity;
    - renamed Generator._paste_image() as Generator._paste() for simplicity;
    - other minor improvements.

--- app/image.py
+++ app/image.py
@@ -7,6 +7,9 @@ from textwrap import wrap
 # Third party
 from PIL import Image, ImageDraw, ImageFont
 
+# Local
+from offer import Offer
+
 
 # --- Global configuration ---
 
@@ -22,98 +25,134 @@ logger.addHandler(hdlr=handler)
 # Global variables
 DEFAULT_FONT_PATH = './resources/fonts/open_sans.ttf'
 DEFAULT_FONT_SIZE = 10
-DEFAULT_IMAGE_OUTPUT_PATH = './temp/image.png'
+DEFAULT_OFFER_TITLE_MAX_LENGTH = 50
+DEFAULT_OUTPUT_FILE_NAME = 'image.png'
+DEFAULT_OUTPUT_FILE_FOLDER = './temp/'
+DEFAULT_IG_POST_TEMPLATE_PATH = './resources/templates/story-720x1280-blue.png'
 
 
 # --- The Generator class ---
 class Generator:
 
-    # Class logger setup
-    _class_logger = logging.getLogger(__name__).getChild(__qualname__)
-    _class_logger.info('Class logger set up.')
+    # --- Magic methods ---
 
+    # __init__
+    def __init__(self,
+                 ig_post_template_path:str):
 
+        # Instance logger setup
+        self._logger = logging.getLogger(__name__)\
+                              .getChild(self.__class__.__name__)\
+
+        # Validade story template
+        if not ig_post_template_path:
+            raise ValueError('Missing Instagram post template path.')
+
+        # Set object variables
+        self.ig_post_template_path = ig_post_template_path
+    
+    
     # --- Public methods ---
 
-    # Create Instagram offer image
+    # Get Generator
     @classmethod
-    def create_instagram_offer_image(cls,
-                                     template_path:str,
-                                     offer_thumbnail_path:str,
-                                     offer_title:str,
-                                     offer_price_from:float,
-                                     offer_price_for:float,
-                                     dest_path:str
-                                     ) -> str:
-        
+    def get(cls, 
+            ig_post_template_path:str = DEFAULT_IG_POST_TEMPLATE_PATH
+            ):
+
+        # Return Generator object
+        logger.info('Getting Generator object...')
+        return Generator(ig_post_template_path=ig_post_template_path)
+
+
+    # Create Instagram post image
+    def create_ig_post_image(self,
+                             offer:Offer,
+                             output_file_name:str = DEFAULT_OUTPUT_FILE_NAME,
+                             output_file_folder:str = DEFAULT_OUTPUT_FILE_FOLDER
+                             ) -> str:
+
         # Create new offer image from template
-        cls._load_image(source_path=template_path)
+        self._logger.info('Creating new post image from template...')
+        self._new_image_from_template()
 
-        # Add offer thumbnail to offer image
-        cls._paste_image(source_path=offer_thumbnail_path,
-                         pos_x=40,
-                         pos_y=130,
-                         size_x=640,
-                         size_y=640)
+        # Add offer thumbnail to post image
+        self._logger.info('Adding offer thumbnail to post image...')
+        self._paste(source_path=offer.thumbnail,
+                    x=40,
+                    y=130,
+                    width=640,
+                    heigth=640)
 
-        # Add offer title to offer image
-        cls._add_offer_title(title=offer_title,
-                             title_x=27.5, # (10 + 17.5)
-                             title_y=827.5, # (100 + 700 + 20 + 7.5)
-                             title_font_size=40,
-                             title_max_length=50)
+        # Add offer title to post image
+        self._logger.info('Adding offer title to post image...')
+        self._add_offer_title(x=27.5, # (10 + 17.5)
+                              y=827.5, # (100 + 700 + 20 + 7.5)
+                              title=offer.title,
+                              title_max_length=50,
+                              font_size=40,
+                              text_align='left')
 
-        # Add offer price to offer image
-        cls._add_offer_price(price_from=offer_price_from,
-                             price_to=offer_price_for,
-                             price_x=27.5, # (10+17.5)
-                             price_y=982.5, # (100 + 700 + 15 + 140 + 15 + 12.5)
-                             price_font_size=37.5)
+        # Add offer price to post image
+        self._logger.info('Adding offer price to post image...')
+        self._add_offer_price(x=27.5, # (10 + 17.5)
+                              y=982.5, # (100 + 700 + 15 + 140 + 15 + 12.5))
+                              price_now=offer.price_now,
+                              price_before=offer.price_before,
+                              font_size=40,
+                              text_align='left')
 
-        # Save image as image file and return destination path
-        return cls._save_image_as(dest_path=dest_path)        
+        # Save offer image as PNG file
+        self._logger.info('Saving post image as PNG file...')
+        output_file_path = self._save_image_as(output_file_name,
+                                               output_file_folder)
+
+        # Return output file path
+        return output_file_path
 
 
-    # --- Helper methods ---
+# --- Helper methods ---
 
-    # Add offer price
-    @classmethod
-    def _add_offer_price(cls, 
-                         price_from:float,
-                         price_to:float,
-                         price_x:float,
-                         price_y:float,
-                         price_font_size:float
+    # Add offer price to current image
+    def _add_offer_price(self,
+                         x:float,
+                         y:float,
+                         price_now:float,
+                         price_before:float|None = None,
+                         font_size:float = DEFAULT_FONT_SIZE,
+                         text_align:str = 'left'
                          ) -> None:
         
         # Prepare price text
-        if (not price_from) or (price_from <= price_to):
-            price_text = f'Por apenas R$ {f'{price_to:.2f}'.replace('.',',')}'
+        if not price_before:
+            price_text = f'Por apenas R$ {f'{price_now:.2f}'.replace('.',',')}'
         else:
-            price_text = f'De {f'{price_from:.2f}'.replace('.',',')} '\
-                         f'por apenas {f'{price_to:.2f}'.replace('.',',')}'        
+            price_text = f'De {f'{price_before:.2f}'.replace('.',',')} '\
+                         f'por apenas {f'{price_now:.2f}'.replace('.',',')}'        
 
         # Add price text to offer image
-        cls._insert_textbox(text=price_text,
-                            x=price_x,
-                            y=price_y,
-                            font_size=price_font_size)
-
+        self._insert_textbox(price_text,
+                             x, y,
+                             font_size, 
+                             text_align)
+        
         # Return nothing
         return None
 
 
     # Add offer title to current image
-    @classmethod
-    def _add_offer_title(cls, 
-                         title:str, 
-                         title_x:float, 
-                         title_y:float, 
-                         title_font_size:float, 
-                         title_max_length:int
+    def _add_offer_title(self,
+                         x:float,
+                         y:float,
+                         title:str,
+                         title_max_length:int = DEFAULT_OFFER_TITLE_MAX_LENGTH,
+                         font_size:float = DEFAULT_FONT_SIZE,
+                         text_align:str = 'left'
                          ) -> None:
         
         # Clip offer title if it exceeds max length
+        self._logger.debug(f'Clipping offer title "{title}" '\
+                           f'to max length ({title_max_length})...')
         if len(title) > title_max_length:
             if title[title_max_length-4]==' ':
                 title = f'{title[:title_max_length-4]}...'
@@ -122,26 +161,26 @@ class Generator:
 
         # Break title into multiple lines
         title = '\n'.join(wrap(text=title, width=30))
+        self._logger.debug(f'Clipped offer title to "{title}".')
 
         # Insert offer title into current offer image
-        cls._insert_textbox(text=title,
-                            x=title_x,
-                            y=title_y,
-                            font_size=title_font_size)
+        self._insert_textbox(title, 
+                             x, y, 
+                             font_size, 
+                             text_align)
 
         # Return nothing
         return None
 
 
     # Insert textbox
-    @classmethod
-    def _insert_textbox(cls,
+    def _insert_textbox(self,
                         text:str,
                         x:float,
                         y:float,
+                        font_size:float = DEFAULT_FONT_SIZE,
                         text_align:str = 'left',
                         font_path:str = DEFAULT_FONT_PATH,
-                        font_size:float = DEFAULT_FONT_SIZE
                         ) -> None:
         
         # Set the font
@@ -150,65 +189,70 @@ class Generator:
                                        encoding='unic')
 
         # Add textbox to offer image
-        draw = ImageDraw.Draw(im=cls._current_image)
+        self._logger.debug(f'Inserting textbox at (x,y)=({x}, {y}) '\
+                           f'with text "{text}"...')
+        draw = ImageDraw.Draw(im=self._current_image)
         draw.multiline_text(xy=(x, y),
                             text=text,
                             fill=(0, 0, 0),
                             font=text_font,
                             align=text_align,
                             stroke_width=0)
+        self._logger.debug('Inserted textbox.')
 
         # Return nothing
         return None
 
 
-    # Load image
-    @classmethod
-    def _load_image(cls, source_path:str) -> None:
+    # New image from template
+    def _new_image_from_template(self) -> None:
         
-        # Load image from source path
-        loaded_image = Image.open(fp=source_path)
+        # Create new PIL Image from template
+        new_image = Image.open(self.ig_post_template_path)
 
-        # Set loaded image as generator's current image
-        cls._current_image = loaded_image
-        
+        # Set new image as object current image
+        self._current_image = new_image
+
         # Return nothing
         return None
 
 
     # Paste image into current image
-    @classmethod
-    def _paste_image(cls, 
-                     source_path:str,
-                     pos_x:int,
-                     pos_y:int,
-                     size_x:int,
-                     size_y:int
-                     ) -> None:
+    def _paste(self, 
+               source_path:str,
+               x:int,
+               y:int,
+               width:int,
+               heigth:int
+               ) -> None:
         
-        # Load source image
-        source_image = Image.open(fp=source_path)
+        # Load source image as PIL Image
+        im_src = Image.open(fp=source_path)
 
-        # Resize source image 
-        source_image = source_image.resize(size=(size_x, size_y))
-
+        # Resize source image
+        im_src = im_src.resize(size=(width, heigth))
+        
         # Paste source image into current image
-        cls._current_image.paste(im=source_image,
-                                 box=(pos_x, pos_y))
+        self._current_image.paste(im=im_src, box=(x,y))
 
         # Return nothing
         return None
 
 
     # Save current image as
-    @classmethod
-    def _save_image_as(cls,
-                       dest_path = DEFAULT_IMAGE_OUTPUT_PATH
-                       ) -> None:
+    def _save_image_as(self,
+                       output_file_name:str = DEFAULT_OUTPUT_FILE_NAME,
+                       output_file_folder:str = DEFAULT_OUTPUT_FILE_FOLDER,
+                       ) -> str:
 
-        # Save current image to file path
-        cls._current_image.save(fp=dest_path)
+        # Set destination path
+        output_file_path = f'{output_file_folder}{output_file_name}'
 
-        # Return file path
-        return dest_path
+        # Save current image to output file path
+        self._logger.debug(f'Saving current image to "{output_file_path}"...')
+        self._current_image.save(fp=output_file_path)
+        self._logger.debug('Saved current image. Return output file path.')
+
+        # Return destination path
+        return output_file_path
 
