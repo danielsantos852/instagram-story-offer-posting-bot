b805fba9e7e6e07056a440eb384d441a3c234085
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-13 16:50:33 -0300

    Update scraping.py
    Changes:
    - removed Offer class (moved to another file);
    - improved Scraper.scrape_amazon_offer();
    - implemented proper logging;
    - other minor improvements.

--- app/scraping.py
+++ app/scraping.py
@@ -1,15 +1,18 @@
 # --- Imports ---
 
 # Standard
+from io import BytesIO
+import requests
 import logging
 
 # Third party
+from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import NoSuchElementException
 
+# Local
+from offer import Offer
 
 # --- Global Configuration ---
 
@@ -21,80 +24,9 @@ formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(me
 handler.setFormatter(fmt=formatter)
 logger.addHandler(hdlr=handler)
 
-
-# --- The Offer class ---
-class Offer:
-    
-    # --- Magic methods ---
-
-    # __init__
-    def __init__(self,
-                 offer_url:str,
-                 offer_title:str,
-                 offer_thumbnail:str,
-                 offer_price_now:float,
-                 offer_price_before:float|None,
-                 offer_discount_rate:float|None,
-                 ):
-
-        # Validate offer url
-        if not offer_url:
-            raise ValueError("Missing offer url.")
-
-        # Validate offer name
-        if not offer_title:
-            raise ValueError("Missing offer title.")
-
-        # Validate offer thumbnail
-        if not offer_thumbnail:
-            raise ValueError("Missing offer thumbnail.")
-
-        # Validate offer "now" price
-        if not offer_price_now:
-            raise ValueError('Missing offer "now" price.')
-
-        # Set object attributes
-        self.offer_url = offer_url
-        self.offer_title = offer_title
-        self.offer_thumbnail = offer_thumbnail
-        self.offer_price_now = offer_price_now
-        self.offer_price_before = offer_price_before
-        self.offer_discount_rate = offer_discount_rate
-
-
-    # __str__
-    def __str__(self) -> str:
-        return f'------------------------ Offer Info ------------------------\n'\
-               f'URL:            {self.offer_url}\n'\
-               f'Title:          {self.offer_title}\n'\
-               f'Thumbnail:      {self.offer_thumbnail}\n'\
-               f'"Now" price:    {self.offer_price_now}\n'\
-               f'"Before" price: {self.offer_price_before}\n'\
-               f'Discount rate:  {self.offer_discount_rate}\n'\
-               f'------------------------------------------------------------'
-
-
-    # --- Public methods ---
-
-    # Get Offer
-    @classmethod
-    def get(cls,
-            offer_url,
-            offer_title,
-            offer_thumbnail,
-            offer_price_now,
-            offer_price_before,
-            offer_discount_rate
-            ):
-        
-        # Return Offer object
-        return Offer(offer_url=offer_url,
-                     offer_title=offer_title,
-                     offer_thumbnail=offer_thumbnail,
-                     offer_price_now=offer_price_now,
-                     offer_price_before=offer_price_before,
-                     offer_discount_rate=offer_discount_rate)
-
+# Global variables
+DEFAULT_THUMBNAIL_DEST_FILE_NAME = 'thumbnail.png'
+DEFAULT_THUMBNAIL_DEST_FOLDER = './temp/'
 
 # --- The Scraper class ---
 class Scraper:
@@ -119,74 +51,87 @@ class Scraper:
     def get(cls):
         
         # Return Scraper object
+        logger.info('Getting Scraper object...')
         return Scraper()
 
 
     # Scrape an amazon.com.br offer
     def scrape_amazon_offer(self,
-                            url:str
+                            url:str,
+                            thumbnail_dest_file_name:str = DEFAULT_THUMBNAIL_DEST_FILE_NAME,
+                            thumbnail_dest_folder:str = DEFAULT_THUMBNAIL_DEST_FOLDER
                             ) -> Offer:
 
         # Create a web driver
+        self._logger.info('Creating webdriver...')
         self._create_webdriver()
 
         # Visit offer url
         offer_url = url
-        print('GET-ing offer url...')
+        self._logger.info('Visiting offer webpage...')
         self.driver.get(url=offer_url)
-        print('GET-ed offer url.')
 
         # Get offer title
-        print('Getting offer title...')
+        self._logger.info('Getting offer title...')
         offer_title = self.driver.find_element(by=By.ID, value='productTitle').text.strip()
-        print(f'offer_title = "{offer_title}"')
+        self._logger.debug(f'offer_title = "{offer_title}"')
 
-        # Get offer thumbnail
-        print('Getting offer thumnail...')
+        # Get offer thumbnail url
+        self._logger.info('Getting offer thumbnail source url...')
         _ = self.driver.find_element(by=By.ID, value='imgTagWrapperId')
         _ = _.find_element(by=By.TAG_NAME, value='img')
-        offer_thumbnail = _.get_attribute(name='src')
-        print(f'offer_thumbnail = "{offer_thumbnail}"')
+        url = _.get_attribute(name='src')
+
+        # Download offer thumbnail, save file path
+        self._logger.info('Downloading offer thumbnail...')
+        response = requests.get(url)
+        image = Image.open(BytesIO(response.content))
+        offer_thumbnail = f'{thumbnail_dest_folder}{thumbnail_dest_file_name}'
+        image.save(fp=offer_thumbnail)
+        self._logger.debug(f'offer_thumbnail = "{offer_thumbnail}"')
 
         # Get "now" price
-        print('Getting offer "now" price...')
+        self._logger.info('Getting offer "now" price...')
         element = self.driver.find_element(by=By.ID, value='corePriceDisplay_desktop_feature_div')
         price_whole = element.find_element(by=By.CLASS_NAME, value='a-price-whole').text.strip()
         price_fraction = element.find_element(by=By.CLASS_NAME, value='a-price-fraction').text.strip()
         offer_price_now = float(f'{price_whole}.{price_fraction}')
-        print(f'offer_price_now = {offer_price_now}')
+        self._logger.debug(f'offer_price_now = {offer_price_now}')
 
         # Get "before" price
         try:
-            print('Getting offer "before" price...')
+            self._logger.info('Getting offer "before" price...')
             _ = element.find_element(by=By.CLASS_NAME, value='a-spacing-small')
             _ = _.find_element(by=By.CLASS_NAME, value='a-text-price').text
             offer_price_before = float(_.replace('R$','').replace(',','.'))
-            print(f'offer_price_before = {offer_price_before}')
+            self._logger.debug(f'offer_price_before = {offer_price_before}')
         except NoSuchElementException:
-            print('Offer "before" price not found, set to None.')
             offer_price_before = None
+            self._logger.debug('Offer "before" price not found, '\
+                               'offer_price_before set to None.')
 
         # Get discount rate
         try:
-            print('Getting offer discount rate...')
+            self._logger.info('Getting offer discount rate...')
             _ = element.find_element(by=By.CLASS_NAME, value='savingsPercentage').text
             offer_discount_rate = float(_.replace('-','').replace('%',''))/100
-            print(f'offer_discount_rate = {offer_discount_rate}')
+            self._logger.debug(f'offer_discount_rate = {offer_discount_rate}')
         except NoSuchElementException:
-            print('Offer discount rate not found, set to None.')
             offer_discount_rate = None
+            self._logger.debug('Offer discount not found, offer_discount_rate set to None.')
 
         # Quit web driver
+        self._logger.info('Deleting webdriver...')
         self._delete_webdriver()
 
         # Return Offer object
-        return Offer(offer_url=offer_url,
-                     offer_title=offer_title,
-                     offer_thumbnail=offer_thumbnail,
-                     offer_price_now=offer_price_now,
-                     offer_price_before=offer_price_before,
-                     offer_discount_rate=offer_discount_rate)
+        self._logger.info('Returning Offer object...')
+        return Offer(url=offer_url,
+                     title=offer_title,
+                     thumbnail=offer_thumbnail,
+                     price_now=offer_price_now,
+                     price_before=offer_price_before,
+                     discount_rate=offer_discount_rate)
 
 
     # --- Helper methods ---
@@ -195,7 +140,6 @@ class Scraper:
     def _create_webdriver(self):
 
         # Set Chrome webdriver options and start it
-        print('Creating webdriver...')
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         options.add_experimental_option(
@@ -205,7 +149,6 @@ class Scraper:
             }
         )
         self.driver = webdriver.Chrome(options=options)
-        print('Created webdriver.')
 
         # Return nothing
         return None
@@ -213,17 +156,19 @@ class Scraper:
 
     # Delete webdriver
     def _delete_webdriver(self, 
-                          driver = None):
+                          driver:webdriver.Chrome|None = None):
 
         # If external driver passed, close it
         if driver:
-            print(f'Quitting specified web driver...')
+            self._logger.debug(f'Quitting specified webdriver...')
             driver.quit()
+
         # Else, close self.webdriver
         else:
-            print(f'Quitting object web driver...')
+            self._logger.debug(f'Quitting scraper webdriver...')
             self.driver.quit()
-        print(f'Quitted web driver.')
 
+        self._logger.debug('Quitted webdriver.')
+        
         # Return nothing
         return None
