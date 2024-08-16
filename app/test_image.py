87a36c98875bdb17c3ac2275903cabcae1412437
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-16 19:03:34 -0300

    Update test_image.py

--- app/test_image.py
+++ app/test_image.py
@@ -1,6 +1,7 @@
 # Imports
 from image import Generator
 from offer import Offer
+from test_offer import get_fake_offer
 
 
 # Main function
@@ -13,30 +14,25 @@ def main():
 # Test Generator.create_offer_post_image()
 def test_create_offer_post_image():
 
-    # Create fake Offer object
-    offer = Offer(url='www.google.com',
-                  title='Título do Produto',
-                  thumbnail='./resources/testing/offer-thumbnail-640x640.png',
-                  price_now=9999.99,
-                  price_before=9999.99,
-                  discount_rate=0.99)
+    # Create fake offer
+    offer = get_fake_offer()
 
     # Get a Generator object
-    generator = Generator.get(ig_post_template_path='./resources/templates/offer-post-template-720x1280.png')
+    generator = Generator.get(offer_post_template='./resources/templates/offer-post-720x1280.png')
 
     # Create post image for fake offer
     generator.create_offer_post_image(offer=offer,
-                                      output_file_name='test-offer-post-image-discount.png',
-                                      output_file_folder='./temp/')
+                                      output_img_name='test-offer-post-image-discount.png',
+                                      output_img_folder='./temp/')
 
     # Remove offer's "before" price and discount rate
     offer.price_before = None
-    offer.discount_rate = None
+    offer.discount = None
 
     # Create post image for fake offer (without discount)
     generator.create_offer_post_image(offer=offer,
-                                      output_file_name='test-offer-post-image-no-discount.png',
-                                      output_file_folder='./temp/')
+                                      output_img_name='test-offer-post-image-no-discount.png',
+                                      output_img_folder='./temp/')
 
 
 # Call main function
