4cbf3bb3a00acd970870ea2993c0fabd1196becf
Daniel Rocha <daniel.rocha13493@gmail.com>
2024-08-19 17:47:29 -0300

    Update test_image.py

--- app/test_image.py
+++ app/test_image.py
@@ -18,21 +18,24 @@ def test_create_offer_post_image():
     offer = get_fake_offer()
 
     # Get a Generator object
-    generator = Generator.get(offer_post_template='./resources/templates/offer-post-720x1280.png')
+    generator = Generator.get(
+        post_img_template='./resources/templates/offer-post-720x1280.png')
 
     # Create post image for fake offer
-    generator.create_offer_post_image(offer=offer,
-                                      output_img_name='test-offer-post-image-discount.png',
-                                      output_img_folder='./temp/')
+    generator.create_offer_post_image(
+        offer=offer,
+        output_img_folder='./temp/',
+        output_img_name='test-offer-post-image-discount.png')
 
     # Remove offer's "before" price and discount rate
     offer.price_before = None
     offer.discount = None
 
     # Create post image for fake offer (without discount)
-    generator.create_offer_post_image(offer=offer,
-                                      output_img_name='test-offer-post-image-no-discount.png',
-                                      output_img_folder='./temp/')
+    generator.create_offer_post_image(
+        offer=offer,
+        output_img_folder='./temp/',
+        output_img_name='test-offer-post-image-no-discount.png')
 
 
 # Call main function
